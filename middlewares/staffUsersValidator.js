636ee8f6945df22099bb688efe0a65d1a2230ecb
neethurithesh <neethuka36@gmail.com>
2025-07-23 02:24:35 +0530

    Update staffUsersValidator.js

--- middlewares/staffUsersValidator.js
+++ middlewares/staffUsersValidator.js
@@ -1,6 +1,6 @@
 const { body } = require('express-validator');
 exports.staffUsersValidator = [
-  body('name').notEmpty().withMessage('Name is required'),
+  body('full_name').notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Valid email is required'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 chars long')
 ];
