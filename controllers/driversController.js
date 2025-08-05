3a45fcb1efcff47bc3c80307f79332fd1aa59521
neethurithesh <neethuka36@gmail.com>
2025-08-06 04:21:24 +0530

    Update driversController.js

--- controllers/driversController.js
+++ controllers/driversController.js
@@ -8,13 +8,42 @@ exports.getAll = (req, res) => {
   });
 };
 
-exports.create = (req, res) => {
+
+exports.create = async (req, res) => {
   const data = req.body;
   const id = uuidv4();
- db.query('INSERT INTO drivers SET ?', data, (err) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.status(201).json({ id, ...data });
-  });
+
+  try {
+   
+    db.query('SELECT * FROM drivers WHERE email = ?', [data.email], async (err, results) => {
+      if (err) return res.status(500).json({ error: err.message });
+
+      if (results.length > 0) {
+        return res.status(400).json({ error: 'Email already exists' });
+      }
+
+     
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+      const newDriver = {
+        id,
+        ...data,
+        password: hashedPassword
+      };
+
+      
+      db.query('INSERT INTO drivers SET ?', newDriver, (err) => {
+        if (err) return res.status(500).json({ error: err.message });
+
+         
+        const { password, ...driverData } = newDriver;
+        res.status(201).json(driverData);
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
 };
 
 exports.update = (req, res) => {
