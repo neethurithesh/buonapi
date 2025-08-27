8b0bd34348c21bfa316d6da549b763bc2b6f04ea
neethurithesh <neethuka36@gmail.com>
2025-08-27 13:52:45 +0530

    phone validate

--- middlewares/driversValidator.js
+++ middlewares/driversValidator.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator');
 exports.driversValidator = [
   body('name').notEmpty(), 
   body('email').isEmail(), 
-  body('phone').isEmail(), 
+  body('phone').notEmpty(), 
 ];
 
 exports.driversLoginValidator = [ 
